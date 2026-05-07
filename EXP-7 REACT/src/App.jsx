ef9b68209e96872362d88da212727725bbf7e742
indeedyashika <indeedyashika@gmail.com>
2026-05-07 20:21:42 +0530

    Add EXP-7 REACT

---
+++ EXP-7 REACT/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Student from './components/Student'
+
+function App() {
+  return (
+       <div className="container">
+      <h1>Student Information</h1>
+      <Student name="YASHIKA NAYAK" course="Computer Science" marks="95" />
+      <Student name="RITA SINGH" course="Data Science" marks="82" />
+      <Student name="GEETA SHARMA" course="Electronics" marks="78" />
+    </div>
+  )
+}
+
+export default App
