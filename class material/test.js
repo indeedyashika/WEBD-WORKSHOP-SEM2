b0df584a773d6ae602e03662b4337086426ab28d
indeedyashika <indeedyashika@gmail.com>
2026-06-10 15:26:47 +0530

    Recovered my classwork

---
+++ class material/test.js
@@ -0,0 +1,39 @@
+{var name1 = "abc";
+console.log(name1);}
+console.log(name1);
+
+//{let name2 = "xyz";
+//console.log(name2);}
+
+{const name3 = "pqr";
+console.log(name3);
+}
+// datatypes
+// string, number, bigint, boolean, null, undefined, symbol
+let names="tspmo";
+console.log(names);
+
+let a=1;
+console.log(a);
+
+let x=12635336n;
+console.log(x);
+
+let b=true;
+console.log(b);
+
+let c=null;
+console.log(c);
+
+let d=undefined;
+console.log(d);
+
+let id=Symbol("id");
+console.log(id);
+//env vars 
+
+//compare only value, compare value and type
+let num1=10;
+let num2="10";
+console.log(num1==num2);
+console.log(num1===num2);
